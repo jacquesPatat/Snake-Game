854beba6b4f0128756c261d190dc59823a3eb72d
Jacques <98026116+jacquesPatat@users.noreply.github.com>
2025-06-29 07:58:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ while game_is_on:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
 
 
 
